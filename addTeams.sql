29f2e4825f0e9723cb3542e94365380a79d30de4
Michał Smorągiewicz <michalgsmoragiewicz@gmail.com>
2023-12-23 23:36:15 +0100

    Dodawanie pierwszych danych

---
+++ addTeams.sql
@@ -0,0 +1,18 @@
+INSERT INTO Teams
+VALUES
+(1, 'Cracovia Kraków'),
+(2, 'Górnik Łęczna'),
+(3, 'Górnik Zabrze'),
+(4, 'Jagiellonia Białystok'),
+(5, 'Korona Kielce'),
+(6, 'Lech Poznan'),
+(7, 'Lechia Gdańsk'),
+(8, 'Legia Warszawa'),
+(9, 'Piast Gliwice'),
+(10, 'Podbeskidzie Bielsko-Biała'),
+(11, 'Pogoń Szczecin'),
+(12, 'Ruch Chorzów'),
+(13, 'Śląsk Wrocław'),
+(14, 'Termalica Bruk-Bet Nieciecza'),
+(15, 'Wisła Kraków'),
+(16, 'Zagłębie Lubin');
