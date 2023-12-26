4e4c9cf558eaad9fc70627158608da03babe415f
Michał Smorągiewicz <michalgsmoragiewicz@gmail.com>
2023-12-26 23:45:52 +0100

    Dodane tabele: Players, Coaches, Stadiums, Teams i uzupełnione wszystkie oprócz Players

---
+++ addData.sql
@@ -0,0 +1,58 @@
+USE ProjectDB
+
+INSERT INTO Stadiums
+VALUES
+(1, 'Stadion im. Józefa Piłsudskiego', 'Kraków', 'ul. Kałuży 1', 14941),
+(2, 'Stadion Górnika Łęczna', 'Łęczna', 'al. Jana Pawła II 13', 7496),
+(3, 'Stadion im. Ernesta Pola', 'Zabrze', 'ul. Roosevelta 21', 5500),
+(4, 'Stadion Miejski', 'Białystok', 'ul. Słoneczna 1', 22386),
+(5, 'Kolporter Arena', 'Kielce', 'ul. Ściegiennego 8', 15500),
+(6, 'INEA Stadion', 'Poznań', 'ul. Bułgarska 17', 41609),
+(7, 'PGE Arena', 'Gdańsk', 'ul. Pokoleń Lechii Gdańsk 1', 41620),
+(8, 'Stadion Miejski im. Józefa Piłsudskiego', 'Warszawa', 'ul. Łazienkowska 3', 31103),
+(9, 'Stadion Miejski', 'Gliwice', 'ul. Okrzei 20', 10037),
+(10, 'Stadion Miejski', 'Bielsko-Biała', 'ul. Rychlińskiego 21', 6692),
+(11, 'Stadion im. Floriana Krygiera', 'Szczecin', 'el. Karłowicza 28', 17048),
+(12, 'Stadion Miejski', 'Chorzów', 'ul. Cicha 6', 10000),
+(13, 'Stadion Miejski', 'Wrocław', 'ul. Śląska 1', 42771),
+(14, 'Stadion Bruk-Bet', 'Nieciecza', 'Nieciecza 150', 5200),
+(15, 'Stadion Miejski im. Henryka Reymana', 'Kraków', 'ul. Reymonta 20', 33130),
+(16, 'Stadion Zagłębia Lubin', 'Lubin', 'ul. Marii Skłodowskiej-Curie 98', 16068);
+
+INSERT INTO Teams
+VALUES
+(1, 'Cracovia Kraków', 1),
+(2, 'Górnik Łęczna', 2),
+(3, 'Górnik Zabrze', 3),
+(4, 'Jagiellonia Białystok', 4),
+(5, 'Korona Kielce', 5),
+(6, 'Lech Poznan', 6),
+(7, 'Lechia Gdańsk', 7),
+(8, 'Legia Warszawa', 8),
+(9, 'Piast Gliwice', 9),
+(10, 'Podbeskidzie Bielsko-Biała', 10),
+(11, 'Pogoń Szczecin', 11),
+(12, 'Ruch Chorzów', 12),
+(13, 'Śląsk Wrocław', 13),
+(14, 'Termalica Bruk-Bet Nieciecza', 14),
+(15, 'Wisła Kraków', 15),
+(16, 'Zagłębie Lubin', 16);
+
+INSERT INTO Coaches
+VALUES
+(1, 'Jacek', 'Zieliński'),
+(2, 'Jurij', 'Szałatow'),
+(3, 'Robert', 'Warzycha'),
+(4, 'Michał', 'Probierz'),
+(5, 'Marcin', 'Brosz'),
+(6, 'Maciej', 'Skorża'),
+(7, 'Jerzy', 'Brzęczek'),
+(8, 'Henning', 'Berg'),
+(9, 'Radosław', 'Latal'),
+(10, 'Dariusz', 'Kubicki'),
+(11, 'Czesław', 'Michniewicz'),
+(12, 'Waldemar', 'Fornalik'),
+(13, 'Tadeusz', 'Pawłowski'),
+(14, 'Piotr', 'Mandrysz'),
+(15, 'Kazimierz', 'Moskal'),
+(16, 'Piotr', 'Stokowiec');
--- createTables.sql
+++ createTables.sql
@@ -1,4 +1,36 @@
-CREATE TABLE Teams(
-    TeamID int PRIMARY KEY,
-    Name VARCHAR(50) NOT NULL
+USE ProjectDB
+
+IF OBJECT_ID(N'dbo.Stadiums', N'U') IS NULL
+CREATE TABLE Stadiums(
+    StadiumID INT PRIMARY KEY,
+    Stadium_Name VARCHAR(50) NOT NULL,
+    City_Name VARCHAR(50) NOT NULL,
+    [Address] VARCHAR(50) NOT NULL,
+    Capacity INT NOT NULL
 )
+GO
+
+IF OBJECT_ID(N'dbo.Teams', N'U') IS NULL
+CREATE TABLE Teams(
+    TeamID INT PRIMARY KEY,
+    Team_Name VARCHAR(50) NOT NULL,
+    StadiumID INT FOREIGN KEY REFERENCES Stadiums
+)
+GO
+
+IF OBJECT_ID(N'dbo.Players', N'U') IS NULL
+CREATE TABLE Players(
+    PlayerID INT PRIMARY KEY,
+    Player_Name VARCHAR(50) NOT NULL,
+    Player_Surname VARCHAR(50) NOT NULL,
+    TeamID INT FOREIGN KEY REFERENCES Teams
+)
+GO
+
+IF OBJECT_ID(N'dbo.Coaches', N'U') IS NULL
+CREATE TABLE Coaches(
+    CoachID INT PRIMARY KEY,
+    Coach_Name VARCHAR(50) NOT NULL,
+    Coach_Surname VARCHAR(50) NOT NULL
+)
+GO
