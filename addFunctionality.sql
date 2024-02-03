6e1aec4cfa0196d3b1b01a4b36114f715008f77f
Michał Smorągiewicz <michalgsmoragiewicz@gmail.com>
2024-02-03 22:54:44 +0100

    Dodane funkcje, procedury i widoki do wstawiania danych do MatchPlayers + widok klasyfikacji strzelców

--- addFunctionality.sql
+++ addFunctionality.sql
@@ -8,13 +8,30 @@ AS
 	WHERE End_Work IS NULL
 GO
 
+CREATE FUNCTION GetPlayersName (@ID INT)
+RETURNS VARCHAR(50)
+AS
+BEGIN
+	RETURN (SELECT (Player_Name + ' ' + Player_Surname) FROM Players WHERE PlayerID = @ID)
+END
+GO
+
+CREATE VIEW TopScorers
+AS
+	SELECT TOP 50 [dbo].[GetPlayersName](MP.PlayerID) Zawodnik, SUM(MP.Goals_Scored) Bramki
+	FROM MatchesPlayers MP JOIN Players P ON MP.PlayerID = P.PlayerID JOIN Teams T ON P.TeamID = T.TeamID
+	WHERE MP.Goals_Scored > 0
+	GROUP BY MP.PlayerID
+	ORDER BY SUM(MP.Goals_Scored) DESC
+GO
+
 CREATE PROCEDURE TeamInfo @ID INT
 AS
 	SELECT T.Team_Name [Nazwa Zespolu], S.Stadium_Name Stadion, CC.Trener 
 	FROM Teams T JOIN Stadiums S ON T.StadiumID = S.StadiumID JOIN CurrentCoaches CC ON T.Team_Name = CC.Zespol
 	WHERE TeamID = @ID
 
-	SELECT (Player_Name + ' ' + Player_Surname) Zawodnicy FROM Players
+	SELECT [dbo].[GetPlayersName](PlayerID) Zawodnicy FROM Players
 	WHERE TeamID = @ID
 GO
 
@@ -45,5 +62,68 @@ AS
 	SELECT [dbo].[TeamNameFromID](M.FirstTeam) Gospodarz, [dbo].[TeamNameFromID](M.SecondTeam) Gosc, FirstTeamScore [' '], SecondTeamScore [' ']
     FROM Matches M JOIN LeagueMatches LM ON M.MatchID = LM.MatchID JOIN Teams T ON T.TeamID = M.FirstTeam
 	WHERE LM.Matchweek = @Week
+GO
+
+CREATE FUNCTION CheckIfPlayerSuspended(@PID INT, @MatchDate DATE)
+RETURNS BIT
+AS
+BEGIN
+	DECLARE @StartDate DATE
+	DECLARE @EndDate DATE
+	-- Pobieramy daty zawieszen gracza i tworzymy kursor ktorym po nich przejdziemy
+	DECLARE CursorSuspensions CURSOR
+	FOR (SELECT Pause_Start, Pause_End FROM OutOfGame WHERE PlayerID = @PID)
+
+	OPEN CursorSuspensions
+	FETCH NEXT FROM CursorSuspensions INTO
+		@StartDate,
+		@EndDate;
+
+	WHILE @@FETCH_STATUS = 0
+		BEGIN
+			-- Gdy data meczu w trakcie zawieszenia zwracamy TRUE
+			IF @MatchDate BETWEEN @StartDate AND @EndDate
+				RETURN 'TRUE'
+			FETCH NEXT FROM CursorSuspensions INTO
+				@StartDate,
+				@EndDate;
+		END
+	RETURN 'FALSE'
+END
+GO
+
+CREATE FUNCTION PlayerPlayedTheGame (@PID INT, @MID INT)
+RETURNS BIT
+AS
+BEGIN
+	-- Pobieramy indeksy zespolu zawodnika oraz uczestnikow danego meczu
+	DECLARE @PlayersTeam INT = (SELECT TeamID FROM Players WHERE PlayerID = @PID)
+	DECLARE @FirstTeam INT = (SELECT FirstTeam FROM Matches WHERE MatchID = @MID)
+	DECLARE @SecondTeam INT = (SELECT SecondTeam FROM Matches WHERE MatchID = @MID)
+	-- Sprawdzamy czy zawodnik wystepuje w ktoryms z zespolow z danego meczu
+	DECLARE @PlayersTeamPlayed BIT = 
+		(SELECT CASE WHEN (@PlayersTeam = @FirstTeam OR @PlayersTeam = @SecondTeam) 
+					THEN 'TRUE' ELSE 'FALSE' END)
+
+	-- Pobieramy date meczu
+	DECLARE @MatchDate DATE = (SELECT Match_Date FROM Matches WHERE MatchID = @MID)
+	-- Sprawdzamy czy zawodnik byl w tym dniu zawieszony
+	DECLARE @PlayerSuspended BIT = [dbo].[CheckIfPlayerSuspended](@PID, @MatchDate)
+
+	IF @PlayersTeamPlayed = 'TRUE' AND @PlayerSuspended = 'False'
+			RETURN 'TRUE'
+	RETURN 'FALSE'
+END
+GO
+
+CREATE PROCEDURE AddPlayerContribution @MatchID INT, @PlayerID INT, @Goals INT, @YellowCards INT, @RedCards INT
+AS
+BEGIN
+	IF [dbo].[PlayerPlayedTheGame](@PlayerID, @MatchID) = 'TRUE'
+		INSERT INTO MatchesPlayers
+		VALUES (@MatchID, @PlayerID, @Goals, @YellowCards, @RedCards)
+	ELSE
+		PRINT 'Zawodnik nie uczestniczyl w meczu'
+END
 GO
 
